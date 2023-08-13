0ee5c5fe478e17942ee1d893e4d55cb46c192bee
Tingyi Wanyan <tiwanyan@gmail.com>
2023-08-13 16:11:55 -0500

    correction

--- utils/process_data.py
+++ utils/process_data.py
@@ -70,8 +70,11 @@ def generate_feature_frame(cell_line_drug: pd.DataFrame):
 				drug_name = drug_names[j]
 				cell_line_name = cell_line_names[i]
 				features = get_cell_line_feature(cell_line_name, drug_name)
-				print(features)
+				#print(features)
 				ic50_value = cell_line_drug.loc[cell_line_drug['Cell_line_Name'] == cell_line_name][drug_name][0]
+				print(drug_name)
+				print(cell_line_name)
+				print(ic50_value)
 				if not features == None:
 					cell_line_name_list.append(cell_line_name)
 					drug_name_list.append(drug_name)
