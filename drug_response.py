c1fb60087a4bc159cfe8f11a6a75a198658044be
Tingyi Wanyan <tiwanyan@gmail.com>
2023-08-29 14:53:52 -0500

    correction

--- drug_response.py
+++ drug_response.py
@@ -1,3 +1,9 @@
+import numpy as np
+from utils.process_data import *
+from sklearn import linear_model
 
+
+#ic50_df = normalize_ic50_drug(drug_cellline_features_clean_df)
+#df_cell_line_drug_ic50_normalized = generate_df_normalized_ic50(drug_cellline_features_clean_df, ic50_df)
 
 
