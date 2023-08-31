1e3e37addf90a1bbf987de2eff9de11ab4a921b0
Tingyi Wanyan <tiwanyan@gmail.com>
2023-08-30 21:46:29 -0500

    add additional test

--- drug_response.py
+++ drug_response.py
@@ -7,7 +7,7 @@ from sklearn import linear_model
 #df_cell_line_drug_ic50_normalized = generate_df_normalized_ic50(drug_cellline_features_clean_df, ic50_df)
 
 reg = linear_model.Ridge()
-cell_line_drug_feature, ic50_list = process_chunck_data(drug_cellline_features_ic50_normalized_df,0,10000)
+cell_line_drug_feature, ic50_list = process_chunck_data(drug_cellline_features_ic50_normalized_df,0,5000)
 
 cell_line_drug_feature_test, ic50_list_test = process_chunck_data(drug_cellline_features_ic50_normalized_df,10000,10899)
 
@@ -17,5 +17,6 @@ prediction_ic50 = reg.predict(cell_line_drug_feature_test)
 
 lst = [prediction_ic50, ic50_list_test]
 
+df = pd.DataFrame()
 
 
---
+++ testing_results/result_analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+df = pd.read_csv('ridge_regression.csv')
