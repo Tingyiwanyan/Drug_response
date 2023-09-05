3172b9fba1c2e9ed4237e827f67c36b65076e9a9
Tingyi Wanyan <tiwanyan@gmail.com>
2023-09-05 12:35:24 -0500

    correction

--- utils/process_data.py
+++ utils/process_data.py
@@ -365,7 +365,7 @@ def genereate_data_feature(gene_expressions: list, drug_one_hot_encodings: list,
 	drug_one_hot_encoding_list = []
 	ic50_list = []
 
-	gene_expression_list = list(map(convert_to_list, gene_expressions))
+	#gene_expression_list = list(map(convert_to_list, gene_expressions))
 	drug_one_hot_encoding_list = list(map(convert_to_list, drug_one_hot_encodings))
 	#ic50_list = list(map(process_ic50, ic50s))
 	#ic50_list = list(map())
