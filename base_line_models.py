c9063d9dad10ead890e0c30e7c9b438cad9b8231
Tingyi Wanyan <tiwanyan@gmail.com>
2023-10-04 11:56:51 -0500

    correction

--- base_line_models.py
+++ base_line_models.py
@@ -5,6 +5,8 @@ from keras.layers import Dense
 from keras.callbacks import EarlyStopping
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
 
 epochs = 30
 
@@ -31,4 +33,50 @@ def shallow_nn(input_dim:float):
 	model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
 
 	return model
+
+
+def shallow_position_wise_nn():
+	"""
+	Testing baseline for single position-wise feed forward nn
+	"""
+	X_input = Input((130, 56))
+	Y_input = Input((5842, 1))
+
+	dense_1 = tf.keras.layers.Dense(50, activation='relu', kernel_regularizer=regularizers.L2(1e-4))
+
+	dense_2 = tf.keras.layers.Dense(50, activation='relu', kernel_regularizer=regularizers.L2(1e-4))
+
+	dense_3 = tf.keras.layers.Dense(500, activation='relu', kernel_regularizer=regularizers.L2(1e-4))
+
+	dense_4 = tf.keras.layers.Dense(50, activation='relu', kernel_regularizer=regularizers.L2(1e-4))
+
+	dense_5 = tf.keras.layers.Dense(1)
+
+
+	flattern = tf.keras.layers.Flatten()
+
+	concatenation_layer = concatenation_layer()
+
+	X = dense_1(X_input)
+	Y = dense_2(Y_input)
+
+	X = flattern(X)
+	Y = flattern(Y)
+
+	Y = concatenation_layer(X,Y)
+
+	Y = dense_3(Y)
+	Y = dense_4(Y)
+	Y = dense_5(Y)
+
+	model = Model(inputs=(X_input, Y_input), outputs=Y)
+
+	model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
+
+	return model
+
+
+
+
+
 
--- drug_response.py
+++ drug_response.py
@@ -15,30 +15,30 @@ train_sample_num, test_sample_num = train_test_split(drug_cellline_features_ic50
 
 gene_expression_filtered.set_index('CCLE_ID',inplace=True)
 
-
+"""
 cell_line_drug_feature, ic50_list, drug_name_list = process_chunck_data(drug_cellline_features_ic50_normalized_df, gene_expression_filtered,
 	train_sample_num)
 cell_line_drug_feature_test, ic50_list_test, drug_name_list_test = process_chunck_data(drug_cellline_features_ic50_normalized_df, gene_expression_filtered,
 	test_sample_num)
-
-
 """
+
+
 gene_expression, drug_one_hot_encoding, ic50_list, drug_name_list,drug_smile_length = process_chunck_data_transformer(drug_cellline_features_ic50_normalized_df, gene_expression_filtered,
 	train_sample_num)
 
-#gene_expression = tf.reshape(gene_expression,[gene_expression.shape[0],gene_expression.shape[1],1])
+gene_expression = tf.reshape(gene_expression,[gene_expression.shape[0],gene_expression.shape[1],1])
 drug_one_hot_encoding = tf.reshape(drug_one_hot_encoding,[drug_one_hot_encoding.shape[0],130,56])
 
 gene_expression_test, drug_one_hot_encoding_test, ic50_list_test, drug_name_list_test,drug_smile_length_test = process_chunck_data_transformer(drug_cellline_features_ic50_normalized_df, gene_expression_filtered,
 	test_sample_num)
 
-#gene_expression_test = tf.reshape(gene_expression_test,[gene_expression_test.shape[0],gene_expression_test.shape[1],1])
+gene_expression_test = tf.reshape(gene_expression_test,[gene_expression_test.shape[0],gene_expression_test.shape[1],1])
 drug_one_hot_encoding_test = tf.reshape(drug_one_hot_encoding_test,[drug_one_hot_encoding_test.shape[0],130,56])
 
 
 train_dataset = tf.data.Dataset.from_tensor_slices(
             (gene_expression, drug_one_hot_encoding, drug_smile_length, np.array(ic50_list)))
-"""
+
 
 
 #testing_gene_expression = np.ones((2000, 5842, 1))
@@ -50,11 +50,14 @@ train_dataset = tf.data.Dataset.from_tensor_slices(
 #k.model_construction()
 #k.model_compile()
 
+model = shallow_position_wise_nn()
+history = model.fit((drug_one_hot_encoding, gene_expression),np.array(ic50_list),batch_size=32, validation_split=0.2, epochs=30)
+
 
 #history = k.model.fit((testing_drug_one_hot, testing_gene_expression, testing_drug_smile_length),testing_ic50,batch_size=5, validation_split=0.2, epochs=1)
-model = shallow_nn(cell_line_drug_feature.shape[1])
+#model = shallow_nn(cell_line_drug_feature.shape[1])
 
-hitory = model.fit(cell_line_drug_feature, ic50_list, validation_split=0.2, epochs=30)
+#hitory = model.fit(cell_line_drug_feature, np.array(ic50_list), validation_split=0.2, epochs=30)
 #cell_line_drug_feature, ic50_list = process_chunck_data(drug_cellline_features_ic50_normalized_df,0,5000)
 
 #cell_line_drug_feature_test, ic50_list_test = process_chunck_data(drug_cellline_features_ic50_normalized_df,10000,10899)
