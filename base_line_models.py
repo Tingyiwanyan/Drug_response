8a2eeb74a66ab2febe146ff6e7d2f295234acb33
Tingyi Wanyan <tiwanyan@gmail.com>
2023-10-04 14:11:24 -0500

    correction

--- base_line_models.py
+++ base_line_models.py
@@ -110,7 +110,7 @@ def base_drug_transformer():
 	score, value = dotproductattention1(X,X,X, enc_valid_lens)
 	att_score = masked_softmax_(score, enc_valid_lens)
 	att_embedding_ = att_embedding(att_score, value)
-	X = r_connection(X, att_embedding_)
+	X = r_connection(value, att_embedding_)
 
 	Y = dense_2(Y_input)
 
@@ -128,6 +128,16 @@ def base_drug_transformer():
 	model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
 
 	return model
+
+
+def att_score_output(input_model):
+	"""
+	Generate intermediate attention score for examination
+	"""
+	att_layer = model.get_layer('dotproductattention')
+	att_output = Model(inputs=input_model.input, outputs = att_layer.output)
+
+	return att_output
 
 
 
