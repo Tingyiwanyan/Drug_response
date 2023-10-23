7d454fd09d20a7312a7025131f84c3b67c13e975
Tingyi Wanyan <tiwanyan@gmail.com>
2023-10-23 17:31:41 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -829,7 +829,7 @@ class drug_transformer_():
 		self.kernel_query = tf.keras.layers.Dense(50, activation='sigmoid', 
 			kernel_regularizer=regularizers.L2(1e-4))
 
-		self.pos_encoding = positionalencoding(50,130)
+		self.pos_encoding = positionalencoding(100,130)
 
 		self.flattern_enco = tf.keras.layers.Flatten()
 		self.flattern_deco = tf.keras.layers.Flatten()
