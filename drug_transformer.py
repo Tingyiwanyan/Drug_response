425a8e8455aed125c2a4d28374bf744debee7735
Tingyi Wanyan <tiwanyan@gmail.com>
2024-04-08 10:18:04 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -635,6 +635,10 @@ class residual_connection(tf.keras.layers.Layer):
 	def call(self, X, Y, **kwargs):
 		#X = tf.math.l2_normalize(X, axis=-1)
 		#Y = tf.math.l2_normalize(Y, axis=-1)
+		print("X shape")
+		print(X.shape)
+		print("Y shape")
+		print(Y.shape)
 		return tf.cast(tf.math.l2_normalize(tf.math.add(X,Y), axis=-1), dtype=tf.float32)
 		#return tf.cast(tf.math.add(X,Y), dtype=tf.float32)
 
