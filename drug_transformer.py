f8864f42ae6fe612b86ff007399d64d41eeb8a51
Tingyi Wanyan <tiwanyan@gmail.com>
2024-04-09 12:11:51 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -727,7 +727,7 @@ class encoder_block(tf.keras.layers.Layer):
 		value = tf.math.l2_normalize(value, axis=-1)
 		att_score = self.masked_softmax(score, if_sparse_max, enc_valid_lens)
 		print(att_score.shape)
-		att_embedding_ = self.att_embedding(att_score, value)#, relative_encoding_lookup=relative_pos_enc, relative_encoding_origin=relative_pos_origin_)
+		att_embedding_ = self.att_embedding(att_score, value, relative_encoding_lookup=relative_pos_enc, relative_encoding_origin=relative_pos_origin_)
 
 		encoder_embedding = self.r_connection(value, att_embedding_)
 		#encoder_embedding = value
