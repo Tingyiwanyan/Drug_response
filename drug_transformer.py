80bab1af1cb7ae473216f936f9a7ba1c2cb9eb6e
Tingyi Wanyan <tiwanyan@gmail.com>
2024-04-18 16:12:17 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -440,8 +440,8 @@ class dotproductattention(tf.keras.layers.Layer):  #@save
 			#print(queries_.shape)
 			#relative_encoding_lookup = tf.expand_dims(relative_encoding_lookup,axis=0)
 			#relative_encoding_lookup = tf.broadcast_to(relative_encoding_lookup,[shape[0],shape[1],shape[1],shape[-1]])
-			scores_position = tf.reduce_sum(tf.multiply(queries_, relative_encoding_lookup), axis=-1)*0.4
-			scores_edge_embedding = tf.reduce_sum(tf.multiply(queries_, edge_type_embedding), axis=-1)*0.4
+			scores_position = tf.reduce_sum(tf.multiply(queries_, relative_encoding_lookup), axis=-1)*0.3
+			scores_edge_embedding = tf.reduce_sum(tf.multiply(queries_, edge_type_embedding), axis=-1)*0.5
 			#print(scores_position.shape)
 			#scores = tf.reduce_sum(tf.multiply(queries_origin, queries_), axis=-1)
 			scores = tf.add(scores_, scores_position)
