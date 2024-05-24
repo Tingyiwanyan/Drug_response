b7b93a50900f17edf6f4265af5c3e2677a14af7c
Tingyi Wanyan <tiwanyan@gmail.com>
2024-05-24 13:29:06 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -379,31 +379,31 @@ class dotproductattention(tf.keras.layers.Layer):  #@save
 
 
 	def build(self, input_shape):
-	    self.kernel_key = self.add_weight(name='kernel_key', shape = (input_shape[-1], self.output_dim),
-	        initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
+		self.kernel_key = self.add_weight(name='kernel_key', shape = (input_shape[-1], self.output_dim),
+			initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
 
-	    b_init = tf.zeros_initializer()
-	    self.b_key = tf.Variable(name='bias_key', 
-	    	initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
+		b_init = tf.zeros_initializer()
+		self.b_key = tf.Variable(name='bias_key', 
+			initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
 
-	    #self.b_key = self.add_weight(name='bias_key',shape = (self.output_dim,),
-            #initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
+		#self.b_key = self.add_weight(name='bias_key',shape = (self.output_dim,),
+		#initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
 
-	    self.kernel_query  = self.add_weight(name='kernel_query', shape = (input_shape[-1], self.output_dim),
-	        initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
+		self.kernel_query  = self.add_weight(name='kernel_query', shape = (input_shape[-1], self.output_dim),
+			initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
 
-	    self.b_query = tf.Variable(name='bias_query', 
-	        initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
+		self.b_query = tf.Variable(name='bias_query', 
+			initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
 
-	    #self.b_query = self.add_weight(name='bias_query',shape = (self.output_dim,),
-            #initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
+		#self.b_query = self.add_weight(name='bias_query',shape = (self.output_dim,),
+		#initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
 
-        if self.if_select_feature == None:
-		    self.kernel_value = self.add_weight(name='kernel_value', shape=(input_shape[-1], self.output_dim),
-		    	initializer=tf.keras.initializers.he_normal(seed=42), trainable=True)
+		if self.if_select_feature == None:
+			self.kernel_value = self.add_weight(name='kernel_value', shape=(input_shape[-1], self.output_dim),
+				initializer=tf.keras.initializers.he_normal(seed=42), trainable=True)
 
-		    self.b_value = tf.Variable(name='bias_value',
-		    	initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
+			self.b_value = tf.Variable(name='bias_value',
+				initial_value=b_init(shape=(self.output_dim,), dtype="float32"), trainable=True)
 
 	    #self.b_value = self.add_weight(name='bias_value',shape = (self.output_dim,),
             #initializer = tf.keras.initializers.RandomNormal(seed=42), trainable = True)
