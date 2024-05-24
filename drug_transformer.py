39a645f49ad4b287f5f2bc471987fbfef4265c7c
Tingyi Wanyan <tiwanyan@gmail.com>
2024-05-24 14:52:43 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -811,7 +811,7 @@ class drug_transformer_():
         """
         global decoder
         """
-        self.decoder_global_1 = decoder_cross_block(30，if_select_feature_=True)
+        self.decoder_global_1 = decoder_cross_block(30, if_select_feature_=True)
         self.decoder_global_2 = decoder_cross_block(30, if_select_feature_=True)
         self.decoder_global_3 = decoder_cross_block(30, if_select_feature_=True)
     
