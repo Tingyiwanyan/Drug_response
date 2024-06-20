e337c73cb0098b3190d6ab5385a8b00aa2695e2e
Tingyi Wanyan <tiwanyan@gmail.com>
2024-06-19 19:12:03 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -671,8 +671,8 @@ class residual_connection(tf.keras.layers.Layer):
     def call(self, X, Y, **kwargs):
         #X = tf.math.l2_normalize(X, axis=-1)
         #Y = tf.math.l2_normalize(Y, axis=-1)
-        return tf.cast(tf.math.l2_normalize(tf.math.add(X,Y), axis=-1), dtype=tf.float32)
-        #return tf.cast(tf.math.add(X,Y), dtype=tf.float32)
+        #return tf.cast(tf.math.l2_normalize(tf.math.add(X,Y), axis=-1), dtype=tf.float32)
+        return tf.cast(tf.math.add(X,Y), dtype=tf.float32)
 
 
 class feed_forward_layer(tf.keras.layers.Layer):
@@ -903,6 +903,7 @@ class drug_transformer_():
         self.r_connection_gene_emb = residual_connection()
         self.r_connection_gene_mutate = residual_connection()
         self.r_connection_multi_deco_gene = residual_connection()
+        self.r_connection_feature = residual_connection()
     
         self.dense_0 = tf.keras.layers.Dense(60, kernel_initializer=initializers.RandomNormal(seed=42),
                                              activation='relu',
@@ -1128,6 +1129,7 @@ class drug_transformer_():
         #Y_key3 = self.dense_6(Y_key3)
         #att_score_global2 = tf.transpose(att_score_global2, perm=[0,2,1])
         Y_global = tf.math.multiply(att_score_global2, Y_key)
+        Y_global = self.r_connection_feature(Y_key, Y_global)
         #Y_global2 = tf.math.multiply(att_score_global2, Y_key2)
         #Y_global3 = tf.math.multiply(att_score_global3, Y_key3)
         #Y = tf.concat([Y_global1, Y_global2, Y_global3],axis=-1)
