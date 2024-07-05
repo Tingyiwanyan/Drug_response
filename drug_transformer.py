2861ce7342940d13956ff6afdedb39b29aaabece
Tingyi Wanyan <tiwanyan@gmail.com>
2024-07-05 14:09:01 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -1124,8 +1124,8 @@ class drug_transformer_():
 
 		#Y = self.dense_15(Y)
 
-		X_global, att_score_global1, Y_value = self.decoder_global_1(X_global, Y, if_sparse_max=False)#, if_select_feature_=None)
-		X_global, att_score_global2, Y_key = self.decoder_global_2(X_global, Y_value, if_sparse_max=False, if_select_feature_=True)
+		X_global, att_score_global1, Y_value, score_cross = self.decoder_global_1(X_global, Y, if_sparse_max=False)#, if_select_feature_=None)
+		X_global, att_score_global2, Y_key, score_cross_global = self.decoder_global_2(X_global, Y_value, if_sparse_max=False, if_select_feature_=True)
 		#X_global3, att_score_global3, Y_key3 = self.decoder_global_3(X_global, Y, if_sparse_max=True, if_select_feature_=True)
 
 		#X_global1, att_score_global1 = self.decoder_global_1(X_global, Y, if_sparse_max=True)
@@ -1158,7 +1158,7 @@ class drug_transformer_():
 		Y = tf.concat([X_global, Y], axis=-1)   
 		Y = self.dense_5(Y)
 
-		self.model = Model(inputs=(X_input, Y_input, enc_valid_lens_, rel_position_embedding, edge_type_embedding, gene_mutation_input，mask_input), outputs=[Y,score_cross_global])
+		self.model = Model(inputs=(X_input, Y_input, enc_valid_lens_, rel_position_embedding, edge_type_embedding, gene_mutation_input，mask_input), outputs=[Y, score_cross_global])
 		#self.model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
 
 		return self.model
