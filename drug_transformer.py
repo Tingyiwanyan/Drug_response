f9f8cfc136ba77c0d2904ccf1125170587defa2c
Tingyi Wanyan <tiwanyan@gmail.com>
2024-07-08 14:59:45 -0500

    correction

--- drug_transformer.py
+++ drug_transformer.py
@@ -974,6 +974,10 @@ class drug_transformer_():
 		                                     kernel_regularizer=regularizers.L2(1e-4),
 		                                     bias_initializer=initializers.Zeros(), name="dense_16")
 
+		self.dense_17 = tf.keras.layers.Dense(1, kernel_initializer=initializers.RandomNormal(seed=42),
+								        	 #kernel_regularizer=regularizers.L2(1e-4),
+								        	 bias_initializer=initializers.Zeros(), name="dense_17")
+
 
 		self.pos_encoding = positionalencoding(30,130)
 
@@ -1166,9 +1170,11 @@ class drug_transformer_():
 		Y = Y_global
 		X_global = self.flattern_global_(X_global)
 		#Y = tf.math.l2_normalize(self.flattern_deco(Y), axis=-1)
+		X_global_bias = self.dense_17(X_global)
 		Y = self.flattern_deco(Y)
 		Y = tf.concat([X_global, Y], axis=-1)   
 		Y = self.dense_5(Y)
+		Y = tf.math.add(Y, X_global_bias)
 
 		self.model = Model(inputs=(X_input, Y_input, enc_valid_lens_, rel_position_embedding, edge_type_embedding, gene_mutation_input, mask_input), outputs=[Y, score_cross_global, X])
 		#self.model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
